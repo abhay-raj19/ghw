bfe3a8d1eab521641e8ed3209e7ddb971cda2dee
Abhay Rajpoot <rajpootabhay423@gmail.com>
2023-07-03 23:50:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,21 @@ hackathons = {
         "type": "In-Person Only"
     }
 }
+Dates = {
+    "GHW: API Week": {
+        "start_date": "2023-04-03 12:00:00",
+        "end_date": "2023-04-10 12:00:00",
+        "location": "Everywhere, Online",
+        "type": "Digital Only"
+    },
+    "Bitcamp": {
+        "start_date": "2023-04-07 12:00:00",
+        "end_date": "2023-04-09 12:00:00",
+        "location": "College Park, Maryland",
+        "type": "In-Person Only"
+    }
+}
+
 
 
 @app.route("/")
